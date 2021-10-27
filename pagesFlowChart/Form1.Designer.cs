c3db09ae15b886c82672ca694df8b554f83e3b0b
P4 <andrpart669@ed.amdsb.ca>
2021-10-27 14:18:14 -0400

    Add project files.

---
+++ pagesFlowChart/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace pagesFlowChart
+{
+    partial class cornMazeBackground
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.Option2Button = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.imageBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // option1Label
+            // 
+            this.option1Label.AutoSize = true;
+            this.option1Label.BackColor = System.Drawing.Color.Transparent;
+            this.option1Label.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.Color.Black;
+            this.option1Label.Location = new System.Drawing.Point(127, 215);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(99, 28);
+            this.option1Label.TabIndex = 0;
+            this.option1Label.Text = "Option1";
+            // 
+            // option2Label
+            // 
+            this.option2Label.AutoSize = true;
+            this.option2Label.BackColor = System.Drawing.Color.Transparent;
+            this.option2Label.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.Color.Black;
+            this.option2Label.Location = new System.Drawing.Point(127, 266);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(99, 28);
+            this.option2Label.TabIndex = 1;
+            this.option2Label.Text = "Option2";
+            // 
+            // option1Button
+            // 
+            this.option1Button.Location = new System.Drawing.Point(5, 215);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(75, 23);
+            this.option1Button.TabIndex = 2;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // Option2Button
+            // 
+            this.Option2Button.Location = new System.Drawing.Point(5, 271);
+            this.Option2Button.Name = "Option2Button";
+            this.Option2Button.Size = new System.Drawing.Size(75, 23);
+            this.Option2Button.TabIndex = 3;
+            this.Option2Button.UseVisualStyleBackColor = true;
+            this.Option2Button.Click += new System.EventHandler(this.Option2Button_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(0, -2);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(799, 114);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.Text = "You and your Best friend are Stuck in a Corn Maze";
+            // 
+            // imageBox
+            // 
+            this.imageBox.Image = global::pagesFlowChart.Properties.Resources.hubs_1829581167_9147f6f937_b;
+            this.imageBox.Location = new System.Drawing.Point(486, 85);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(313, 315);
+            this.imageBox.TabIndex = 5;
+            this.imageBox.Text = "label1";
+            // 
+            // cornMazeBackground
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.Option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Name = "cornMazeBackground";
+            this.Text = "The Corn Maze";
+            this.Load += new System.EventHandler(this.cornMazeBackground_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button Option2Button;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label imageBox;
+    }
+}
+
---
+++ pagesFlowChart/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+namespace pagesFlowChart
+{
+    public partial class cornMazeBackground : Form
+    {
+        int page =  1;
+        Random randGen = new Random();
+        int randValue;
+
+        public cornMazeBackground()
+        {
+            InitializeComponent();
+            outputLabel.Text = "You and your Best friend are Stuck in a Corn Maze";
+            option1Label.Text = "Work Together";
+            option2Label.Text = "Fight for Survival";
+            SoundPlayer crowPlayer = new SoundPlayer(Properties.Resources._138344__unclesigmund__crow);
+            crowPlayer.Play();
+
+
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+
+                page = 2;
+            }
+            else if (page == 2) { page = 5; }
+            else if (page == 3) { page = 100; }
+            else if (page == 4) { page = 99; }
+            else if (page == 5) { page = 6; }
+            else if (page == 6) { page = 7; }
+            else if (page == 7) { page = 10; }
+            else if (page == 8) { page = 100; }
+            else if (page == 9) { page = 100; }
+            else if (page == 10) { page = 100; }
+            else if (page == 11) { page = 100; }
+
+            switch (page)
+            {
+                // set up the cases
+                case 1:
+                    outputLabel.Text = "You and your Best friend are Stuck in a Corn Maze";
+                    option1Label.Text = "Work together to survive?";
+                    option2Label.Text = "Fight for survival?";
+                    
+
+
+                    break;
+                case 2:
+                    outputLabel.Text = "you see a poisonous bug,what do you do?";
+                    option1Label.Text = "Swat it";
+                    option2Label.Text = "Spray it with ur handy bug spray";
+                    
+                    imageBox.Image = Properties.Resources.harvester_ant;
+                    SoundPlayer crowPlayer = new SoundPlayer(Properties.Resources._138344__unclesigmund__crow);
+                    crowPlayer.Stop();
+
+
+                    break;
+                case 3:
+                    outputLabel.Text = "you faught until you both died from fatiuge.Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+                case 4:
+                    outputLabel.Text = "you sprayed it with bug spray";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    outputLabel.Text = "the bug spray was poisonous. You died.Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                   
+
+                    break;
+                case 5:
+                    outputLabel.Text = "you swated it";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    option1Label.Text = "stay quiet";
+                    option2Label.Text = "scream loud";
+                    break;
+                case 6:
+                    outputLabel.Text = "you swated the bug. it hurt! do you scream loud? or try to hide it?";
+                    option1Label.Text = "Stay quiet";
+                    option2Label.Text = "Scream loud";
+
+                    break;
+                case 7:
+                    outputLabel.Text = "you stayed quiet and backed up into a fox. He looks mad. Do you pet him? or run away? ";
+                    option1Label.Text = "pet him";
+                    option2Label.Text = "run away";
+                    imageBox.Image = Properties.Resources.red_fox_walking_on_grass_field_wallpaper;
+                    
+
+                    break;
+                case 8:
+                    outputLabel.Text = "you scream loud and someone hears you. You get rescued.Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    SoundPlayer screamPlayer = new SoundPlayer(Properties.Resources._59164__timtube__scream);
+                    screamPlayer.Play();
+                    break;
+                case 9:
+                    outputLabel.Text = "you run away. He catches up to you and is offended you didn't want to be his friend. He eats you. You die. play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+                case 10:
+                    outputLabel.Text = "you pet him. He is friendly, he sniffs around and leads you to safety.You win.Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+                case 11:
+                    outputLabel.Text = "your luckily a alot stronger than your friend. you won the fight and got out on your own\n You Win!";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+                case 99:
+                    outputLabel.Text = "THANKS FOR PLAYING";
+                    Refresh();
+                    Thread.Sleep(2500);
+                    Application.Exit();
+                    break;
+                case 100:
+                    outputLabel.Text = "sorry did you say you wanted to Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                   
+
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+        private void Option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                randValue = randGen.Next(1, 101);
+                if (randValue < 70)
+                {
+                    page = 3;
+                }
+                else
+                {
+                    page = 11;
+                }
+            }
+            else if (page == 2) { page = 5; }
+            else if (page == 3) { page = 100; }
+            else if (page == 4) { page = 99; }
+            else if (page == 5) { page = 6; }
+            else if (page == 6) { page = 7; }
+            else if (page == 7) { page = 10; }
+            else if (page == 8) { page = 100; }
+            else if (page == 9) { page = 100; }
+            else if (page == 10) { page = 100; }
+            else if (page == 11) { page = 100; }
+
+
+
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You and your Best friend are Stuck in a Corn Maze";
+                    option1Label.Text = "Work together to survive?";
+                    option2Label.Text = "Fight for survival?";
+
+
+
+
+                    break;
+                case 2:
+                    outputLabel.Text = "you see a poisonous bug,what do you do?";
+                    option1Label.Text = "Swat it";
+                    option2Label.Text = "Spray it with ur handy bug spray";
+
+
+
+                    break;
+                case 3:
+                    outputLabel.Text = "you faught until you both died from fatiuge.Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+                case 4:
+                    outputLabel.Text = "you sprayed it with bug spray";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    outputLabel.Text = "the bug spray was poisonous. You died.Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+
+                    break;
+                case 5:
+                    outputLabel.Text = "you swated it. Press a button to move on";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+
+                    break;
+                case 6:
+                    outputLabel.Text = "you swated the bug. it hurt! do you scream loud? or try to hide it?";
+                    option1Label.Text = "Stay quiet";
+                    option2Label.Text = "Scream loud";
+
+                    break;
+                case 7:
+                    outputLabel.Text = "you stayed quiet and backed up into a fox. He looks mad. Do you pet him? or run away? ";
+                    option1Label.Text = "pet him";
+                    option2Label.Text = "run away";
+                    imageBox.Image = Properties.Resources.red_fox_walking_on_grass_field_wallpaper;
+                    break;
+                case 8:
+                    outputLabel.Text = "you scream loud and someone hears you. You get rescued.Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    SoundPlayer screamPlayer = new SoundPlayer(Properties.Resources._59164__timtube__scream);
+                    screamPlayer.Play();
+                    break;
+                case 9:
+                    outputLabel.Text = "you run away. He catches up to you and is offended you didn't want to be his friend. He eats you.You die. Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+                case 10:
+                    outputLabel.Text = "you pet him. He is friendly, he sniffs around and leads you to safety.You win.Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+
+                case 99:
+                    outputLabel.Text = "THANKS FOR PLAYING";
+                    Refresh();
+                    Thread.Sleep(2500);
+                    Application.Restart();
+                    break;
+                case 100:
+                    outputLabel.Text = "sorry did you say you wanted to Play again?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Application.Restart();
+
+
+                    break;
+                case 101:
+                    Refresh();
+                    Thread.Sleep(2000);
+
+                    Application.Exit();
+                    break;
+
+
+                default:
+                    break;
+            }
+        }
+
+        private void cornMazeBackground_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
